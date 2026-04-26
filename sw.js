1c1a9e3ce9f51aca3cde7227cb72f18654fa292d
whitten29dec-prog <whitten29dec@gmail.com>
2026-04-26 18:06:27 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='commerce-v6';const ASSETS=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE='commerce-v7';const ASSETS=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null))));self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))));});
